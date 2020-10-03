9abeb92bab3d528cd068b4b2131a4cd9728318e6
Hexhare <66854340+Hexhare@users.noreply.github.com>
2020-10-02 18:39:07 -0700

    git attribute changes

---
+++ Assets/_Ryan/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab_redSquare, prefab_blackSquare;
+
+    public GameObject[][] boardtiles;
+
+    int height = 8, width = 8
+        
+    
+
+    // Update is called once per frame
+    public void GenerateBoard()
+    {
+        boardtiles = new GameObject(height)[];
+        
+    }
+}
